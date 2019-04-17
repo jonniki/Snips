3fbd206efd96a7d9a9f0b8f8b4fe637cba01fcc9
jonniki <46942739+jonniki@users.noreply.github.com>
2019-04-17 15:36:49 +0200

    Update action-SagHallo.py

--- action-SagHallo.py
+++ action-SagHallo.py
@@ -9,22 +9,9 @@ import io
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
-class SnipsConfigParser(ConfigParser.SafeConfigParser):
-    def to_dict(self):
-        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
-
-
-def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, ConfigParser.Error) as e:
-        return dict()
 
 def subscribe_intent_callback(hermes, intentMessage):
-    conf = read_configuration_file(CONFIG_INI)
+    conf = None
     action_wrapper(hermes, intentMessage, conf)
 
 
