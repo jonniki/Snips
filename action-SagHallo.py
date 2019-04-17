3feb18652e9454e432191eacdaddf31533764fa0
jonniki <46942739+jonniki@users.noreply.github.com>
2019-04-17 16:53:14 +0200

    Update action-SagHallo.py

--- action-SagHallo.py
+++ action-SagHallo.py
@@ -12,12 +12,18 @@ def subscribe_intent_callback(hermes, intentMessage):
 
 
 def action_wrapper(hermes, intentMessage, conf):
-    result_sentence = "Herzlich Willkommen nach Hause, Eugen. Du hast keine neue Emails bekommen."
+    result_sentence = "Herzlich Willkommen nach Hause, mein Herr Eugen. Ich stehe zu Deinen Dienste."
     current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, result_sentence)
+    
+def intent_callback_Tschuess(hermes, intentMessage, conf):
+    result_sentence = "Auf Wiedersehen, mein Sir. Ich stehe zu Deine Dienste."
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, result_sentence)    
 
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("studger:SagHallo", subscribe_intent_callback) \
-            .start()
+        h.subscribe_intent("studger:SagHallo", subscribe_intent_callback)
+        h.subscribe_intent("studger:SagTschuess", intent_callback_Tschuess)
+        h.start()
