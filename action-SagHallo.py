5a4b23846176616239c220d0fc0252606be943c7
jonniki <46942739+jonniki@users.noreply.github.com>
2019-04-17 15:19:25 +0200

    Update action-SagHallo.py

--- action-SagHallo.py
+++ action-SagHallo.py
@@ -37,5 +37,5 @@ def action_wrapper(hermes, intent_message, conf):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("DANBER:beschimpfe", subscribe_intent_callback) \
+        h.subscribe_intent("studger:SagHallo", subscribe_intent_callback) \
             .start()
